76a6d5c092e28eabb86e161d0c8cb2451b4af23f
Ishan Jaiswal <69758646+ishan-jaiswal-cse0247@users.noreply.github.com>
2023-01-23 17:57:35 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import session from 'express-session';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  next();
+});
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/signup', userRouter);
+app.use('/api/name', userRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
